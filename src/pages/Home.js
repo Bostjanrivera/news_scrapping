c656007b3fac76220806c36ebd3c108de8bef330
sebas rivera <bostjanrivera@gmail.com>
2021-05-10 21:10:44 -0500

    project testing with Cypress

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,19 +6,21 @@ const Home = (newsList) => {
           <li><a href="https://bostjanrivera.github.io/news_scrapping">All</a></li>
           <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/physics">Fisica</a></li>
           <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/astronomy">Astronomia</a></li>
-          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/neuroscience">neurociencia</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/neuroscience">Neurociencia</a></li>
           <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/anthropology">Antropologia</a></li>
         </ul>
       </nav>
+      <h1 class="category--title">All News</h1>
       <section class="news">
         <div class="notice_container">
           ${newsList.map(news => `
           <div class="notice-card">
-            <img src="${news.image}" alt="notice image">
+            <figure>
+              <img src="${news.image}" alt="notice image">
+            </figure>
             <div class="notice-card_text">
               <a href="https://bostjanrivera.github.io/news_scrapping/#/${news._id.$oid}">${news.title}</a>
-              <p>${news.content} </p>
-              <h4 class="notice-card--tag">${news.tag}</h4>
+              <h4 class="notice-card--tag">${news.date}</h4>
             </div>
           </div>
           `).join('')}
--- src/pages/ViewCategory.js
+++ src/pages/ViewCategory.js
@@ -1,29 +1,31 @@
 import Resources from '../resources/Resources'
 
 const ViewCategory = async () => {
-  const category = location.hash.slice(1).toLocaleLowerCase().split('/')[2]
-  const newsList = await Resources.getNewsByTag(category)
+  const category = location.hash.slice(1).toLocaleLowerCase().split('/')
+  const newsList = await Resources.getNewsByTag(category[2])
   console.log(newsList)
   const view = `
     <div class="main-home">
       <nav>
         <ul class="nav">
-          <li><a href="/">All</a></li>
-          <li><a href="/#/category/physics">Fisica</a></li>
-          <li><a href="/#/category/astronomy">Astronomia</a></li>
-          <li><a href="/#/category/neuroscience">neurociencia</a></li>
-          <li><a href="/#/category/antropologhy">Antropologia</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping">All</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/physics">Fisica</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/astronomy">Astronomia</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/Neuroscience">neurociencia</a></li>
+          <li><a href="https://bostjanrivera.github.io/news_scrapping/#/category/anthropology">Antropologia</a></li>
         </ul>
       </nav>
+      <p class="category--title"><span>${category[1]}: </span> ${category[2]} </p>
       <section class="news">
         <div class="notice_container">
           ${newsList.map(news => `
           <div class="notice-card">
-            <img src="${news.image}" alt="notice image">
+            <figure>
+              <img src="${news.image}" alt="notice image">
+            </figure>
             <div class="notice-card_text">
               <a href="https://bostjanrivera.github.io/news_scrapping/#/${news._id.$oid}">${news.title}</a>
-              <p>${news.content} </p>
-              <h4 class="notice-card--tag">${news.tag}</h4>
+              <h4 class="notice-card--tag">${news.date}</h4>
             </div>
           </div>
           `).join('')}
